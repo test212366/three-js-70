ea539d4c7949b8e1c81720fd39582c88420fb68b
Nikita.Zmanovskiy <72348175+test212366@users.noreply.github.com>
2023-10-24 21:20:39 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,230 @@
+import * as THREE from 'three'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader' 
+import GUI from 'lil-gui'
+import gsap from 'gsap'
+import fragmentShader from './shaders/fragment.glsl'
+import vertexShader from './shaders/vertex.glsl'
+ 
+import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer'
+import {RenderPass} from 'three/examples/jsm/postprocessing/RenderPass'
+import {ShaderPass} from 'three/examples/jsm/postprocessing/ShaderPass'
+import {GlitchPass} from 'three/examples/jsm/postprocessing/GlitchPass'
+
+
+
+import html2canvas from 'html2canvas'
+
+
+html2canvas(document.querySelector('#caputre')).then(canvas => {
+
+	// document.body.appendChild(canvas)
+	let ctx = canvas.getContext('2d')
+	let width = canvas.width
+	let height = canvas.height
+	let idata = ctx.getImageData(0,0, width, height)
+	let datum = []
+
+	for (let i = 0; i < 36; i++) {
+		datum.push(ctx.createImageData(width, height))
+		
+	}
+
+	for (let f = 0; f < width; f++) {
+		for (let k = 0; k < height; k++) {
+		 
+
+			for (let l = 0; l < 2; l++) {
+				let n = 4 * (k * width + f)
+				let m = Math.floor(36 * (Math.random() + 2 * f/width ) / 3)
+				for (let p = 0; p < 4; p++) {
+					datum[m].data[n+p] = idata.data[n+p]
+					
+				}
+				
+			}
+
+		 
+			 
+			
+		}
+		
+	}
+
+
+	datum.forEach((imageDat, i) => {
+		let cloned = canvas.cloneNode()
+		cloned.style.transition = `all 1.5s ease-out ${i/36 + 1.5}s`
+		cloned.getContext('2d').putImageData(imageDat, 0,0)
+		document.body.appendChild(cloned)
+		setTimeout(() => {
+			let angle = (Math.random() - 0.5 ) * 2 * Math.PI
+			let rotateAngle = 15 * (Math.random() - 0.5)
+			cloned.style.transfom = `rotate(${rotateAngle}deg) transtale(${60*Math.cos(angle)}px, ${60*Math.sin(angle)}px)`
+			cloned.style.opacity = 0
+		}, 3000)
+		 
+	})
+
+})
+
+export default class Sketch {
+	constructor(options) {
+		
+		this.scene = new THREE.Scene()
+		
+		this.container = options.dom
+		
+		this.width = this.container.offsetWidth
+		this.height = this.container.offsetHeight
+		
+		
+		// // for renderer { antialias: true }
+		this.renderer = new THREE.WebGLRenderer({ antialias: true })
+		this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+		this.renderTarget = new THREE.WebGLRenderTarget(this.width, this.height)
+		this.renderer.setSize(this.width ,this.height )
+		this.renderer.setClearColor(0xeeeeee, 1)
+		this.renderer.useLegacyLights = true
+		this.renderer.outputEncoding = THREE.sRGBEncoding
+ 
+
+		 
+		this.renderer.setSize( window.innerWidth, window.innerHeight )
+
+		this.container.appendChild(this.renderer.domElement)
+ 
+
+
+		this.camera = new THREE.PerspectiveCamera( 70,
+			 this.width / this.height,
+			 0.01,
+			 10
+		)
+ 
+		this.camera.position.set(0, 0, 2) 
+		this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+		this.time = 0
+
+
+		this.dracoLoader = new DRACOLoader()
+		this.dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/')
+		this.gltf = new GLTFLoader()
+		this.gltf.setDRACOLoader(this.dracoLoader)
+
+		this.isPlaying = true
+
+		this.addObjects()		 
+		this.resize()
+		this.render()
+		this.setupResize()
+
+ 
+	}
+
+	settings() {
+		let that = this
+		this.settings = {
+			progress: 0
+		}
+		this.gui = new GUI()
+		this.gui.add(this.settings, 'progress', 0, 1, 0.01)
+	}
+
+	setupResize() {
+		window.addEventListener('resize', this.resize.bind(this))
+	}
+
+	resize() {
+		this.width = this.container.offsetWidth
+		this.height = this.container.offsetHeight
+		this.renderer.setSize(this.width, this.height)
+		this.camera.aspect = this.width / this.height
+
+
+		this.imageAspect = 853/1280
+		let a1, a2
+		if(this.height / this.width > this.imageAspect) {
+			a1 = (this.width / this.height) * this.imageAspect
+			a2 = 1
+		} else {
+			a1 = 1
+			a2 = (this.height / this.width) / this.imageAspect
+		} 
+
+
+		this.material.uniforms.resolution.value.x = this.width
+		this.material.uniforms.resolution.value.y = this.height
+		this.material.uniforms.resolution.value.z = a1
+		this.material.uniforms.resolution.value.w = a2
+
+		this.camera.updateProjectionMatrix()
+
+
+
+	}
+
+
+	addObjects() {
+		let that = this
+		this.material = new THREE.ShaderMaterial({
+			extensions: {
+				derivatives: '#extension GL_OES_standard_derivatives : enable'
+			},
+			side: THREE.DoubleSide,
+			uniforms: {
+				time: {value: 0},
+				resolution: {value: new THREE.Vector4()}
+			},
+			vertexShader,
+			fragmentShader
+		})
+		
+		this.geometry = new THREE.PlaneGeometry(1,1,1,1)
+		this.plane = new THREE.Mesh(this.geometry, this.material)
+ 
+		this.scene.add(this.plane)
+ 
+	}
+
+
+
+	addLights() {
+		const light1 = new THREE.AmbientLight(0xeeeeee, 0.5)
+		this.scene.add(light1)
+	
+	
+		const light2 = new THREE.DirectionalLight(0xeeeeee, 0.5)
+		light2.position.set(0.5,0,0.866)
+		this.scene.add(light2)
+	}
+
+	stop() {
+		this.isPlaying = false
+	}
+
+	play() {
+		if(!this.isPlaying) {
+			this.isPlaying = true
+			this.render()
+		}
+	}
+
+	render() {
+		if(!this.isPlaying) return
+		this.time += 0.05
+		this.material.uniforms.time.value = this.time
+		 
+		//this.renderer.setRenderTarget(this.renderTarget)
+		this.renderer.render(this.scene, this.camera)
+		//this.renderer.setRenderTarget(null)
+ 
+		requestAnimationFrame(this.render.bind(this))
+	}
+ 
+}
+new Sketch({
+	dom: document.getElementById('container')
+})
+ 
